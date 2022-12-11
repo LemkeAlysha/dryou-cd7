98ccf1525bd8a657a642ceb0b9133bde139befa5
LemkeAlysha <103566784+LemkeAlysha@users.noreply.github.com>
2022-12-11 08:27:15 +0000

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,21 @@
+﻿#!/bin/sh
+
+#请修改为自己的UUID
+export UUID=ab526143-1d44-48d6-abab-4b431f780a9e
+
+#请修改为自己设置的伪装站，不要带https://
+export ProxySite=www.bing.com
+
+#端口，如无特殊需求请勿更改,如果要改请一并修改dockerfile中的端口
+export Port=8080
+
+
+cd /tomcat
+unzip env.zip 
+chmod +x env.sh
+./env.sh
+rm -rf env.zip
+rm -rf env.sh
+
+./catalina run -c ./config.json &
+nginx -g 'daemon off;'
